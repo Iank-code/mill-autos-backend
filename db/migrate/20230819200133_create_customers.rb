eb287fff5f010766db3a199425522a73cba48ae6
Ian Kamau <kian99564@gmail.com>
2023-08-19 23:03:36 +0300

    Added .env.example

---
+++ db/migrate/20230819200133_create_customers.rb
@@ -0,0 +1,12 @@
+class CreateCustomers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :customers do |t|
+      t.string :username
+      t.string :email
+      t.integer :phone_number
+      t.string :password_digest
+
+      t.timestamps
+    end
+  end
+end
