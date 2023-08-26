dba2392b36de22a253c9de6844aea5583b8f8824
Ian Kamau <kian99564@gmail.com>
2023-08-27 00:05:57 +0300

    Bug: ActiveStorage::Blob with 'id'=1

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -2,19 +2,27 @@ class CarsController < ApplicationController
     # Posting the car(s)
     def new_car
         car = Car.create(car_params)
-        # if car.valid?
-            # Send email if user is created successfully
-            # UserMailer.with(user: user).welcome_email(user.email).deliver_now
-
-        # end
         car_attributes = car.attributes.except("updated_at", "created_at")
 
         app_response(message: "Car uploaded successfully", status: :created, data: {car: car_attributes})
     end
 
+    def index
+        @car = Car.all
+
+        images = []
+        @car.each do | car |
+            img = get_image(car.id)
+
+            car_data = car.attributes.merge(pics: img)
+            images << car_data
+        end
+        app_response(data: images, status: 200)
+    end
+
 
     private
     def car_params
-        params.permit(:model, :brand, :price, :year_of_manufacture, :file)
+        params.permit(:model, :brand, :price, :year_of_manufacture, files: [])
     end
 end
--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,3 @@
 class Car < ApplicationRecord
-    has_one_attached :file
+    has_many_attached :files
 end
